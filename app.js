76f3dfbd6755e1365e370305a6c9b8ad8374fd4a
its-KT <100306110+its-KT@users.noreply.github.com>
2024-05-08 19:57:44 +0530

    createElement changes

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-console.log("hello");
-alert("3000");
+// Write your code below:
+const head = document.createElement('h3');
+const headt = document.createTextNode('Buy high quality organic fruits online');
+head.appendChild(headt);
+const divv = document.getElementsByTagName('div');
+divv[0].appendChild(head);
+head.style.fontStyle="italic";
+
+
+const totalFruitsPara = document.createElement('p');
+totalFruitsPara.textContent = "Total fruits: 4";
+totalFruitsPara.id = "fruits-total";
+divv[1].insertBefore(totalFruitsPara, divv[1].firstChild);
